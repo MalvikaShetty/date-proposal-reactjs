9498e57851fc914a920a45dbf960770cfb85e390
Malvika <shettymalvika11@gmail.com>
2022-07-09 12:50:45 +0530

    logo and title change

--- src/index.js
+++ src/index.js
@@ -13,10 +13,7 @@ console.log(element);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    {/* <Yes />  */}
-    {/* <Home/> */}
-    
+    <App /> 
   </React.StrictMode>
 );
 
